6a88b73c27c0e57cf818cf9cce8d8e6f358521c6
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2024-01-11 17:39:56 +0900

    MemberServiceImpl.javaの作成

---
+++ src/service/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package service;
+
+import entity.Member;
+
+import java.util.ArrayList;
+
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public String greet(int i) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Member> getAll() {
+        return null;
+    }
+}
