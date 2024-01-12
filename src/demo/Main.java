b6f24c2e0939770cc81d4b533d65ca684b0085d7
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2024-01-12 15:39:48 +0900

    インデント訂正

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-       MemberServiceImpl service = new MemberServiceImpl();
+        MemberServiceImpl service = new MemberServiceImpl();
         System.out.println(service.greet(2));
         System.out.println(service.getAll());
         ArrayList<Member> list = service.getAll();
